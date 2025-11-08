ec34a895053013a0b856b24e96a049f6075e427c
Amir Boutajrit <aboutajrit4@gmail.com>
2025-11-07 16:54:21 -0800

    Fix TypeScript build error

--- src/app/components/AnimatedBackground.tsx
+++ src/app/components/AnimatedBackground.tsx
@@ -29,8 +29,8 @@ export default function AnimatedBackground() {
       opacity: number;
 
       constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = Math.random() * canvas!.width;
+        this.y = Math.random() * canvas!.height;
         this.size = Math.random() * 3 + 1; // 1-4px
         this.speedX = (Math.random() - 0.5) * 0.5; // Slower movement
         this.speedY = (Math.random() - 0.5) * 0.5;
@@ -42,10 +42,10 @@ export default function AnimatedBackground() {
         this.y += this.speedY;
 
         // Wrap around edges
-        if (this.x > canvas.width) this.x = 0;
-        if (this.x < 0) this.x = canvas.width;
-        if (this.y > canvas.height) this.y = 0;
-        if (this.y < 0) this.y = canvas.height;
+        if (this.x > canvas!.width) this.x = 0;
+        if (this.x < 0) this.x = canvas!.width;
+        if (this.y > canvas!.height) this.y = 0;
+        if (this.y < 0) this.y = canvas!.height;
       }
 
       draw() {
